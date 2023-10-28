cb925966abb601271ad140ce6adb29ac9a3c4177
Emma11072012 <144716159+Emma11072012@users.noreply.github.com>
2023-10-28 16:46:09 -0400

    Add files via upload

---
+++ 20231024_Unit_Conversion_homework.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Unit conversion
+#1 Gallon= 8 Pints
+#1 Pint= 2 Cups
+#1 Cup= 8 Fluid Ounces
+#Lets convert gallons to pints and pints to cups and cups to fluid onces.
+
+
+# In[8]:
+
+
+def unit_conversion(gallon):
+    return gallon*8
+
+
+# In[3]:
+
+
+unit_conversion(1)
+
+
+# In[4]:
+
+
+def unit_conversion_2(pints):
+    return pints*2
+
+
+# In[9]:
+
+
+unit_conversion_2(8)
+
+
+# In[6]:
+
+
+def unit_conversion_3(cups):
+    return cups*8
+
+
+# In[10]:
+
+
+unit_conversion_3(16)
+
+
+# In[ ]:
+
+
+
+
